3f5f3a9eb37fa8fa7db8a6c85864ec1a2c57203a
Ramia-aloufi <ramia.aloufi@gmail.com>
2025-09-10 14:18:12 +0300

    Initial Commit

---
+++ Linkst/LinkstApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkstApp.swift
+//  Linkst
+//
+//  Created by ramia n on 18/03/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LinkstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
